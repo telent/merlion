57ec15825edb9780067cf62941eaf8c139c36fe2
Daniel Barlow <dan@telent.net>
2016-08-11 23:57:53 +0100

    massive industrial-scale WIP
    * only pushing this in case of catastrophe, do not even
    think about reading it, never mind using it

--- seed-conf.sh
+++ seed-conf.sh
@@ -1,19 +1,22 @@
 conf(){
     curl -XPUT -d value="$2" http://localhost:2379/v2/keys$1;
 }
-fconf(){
-    curl  -XPUT --data-urlencode $2 http://localhost:2379/v2/keys$1;
-}
 
 site=dev.example.com
 prefix=/conf/merlion/$site
 
 if [ "$1" = "clean" ] ; then
-   etcdctl rm --recursive /conf 
+    etcdctl rm --recursive /conf
+    etcdctl rm --recursive /service
+    exit 0
 fi
 
 conf $prefix/upstream-service-etcd-prefix /service/$site
 conf $prefix/upstream-freshness 60
 conf $prefix/log-format edn
-fconf $prefix/tls-certificate value@test-ssl.crt
-fconf $prefix/tls-private-key value@test-ssl.key
+conf $prefix/listen-address localhost:8088
+
+conf /service/$site/af1/listen-address "localhost:4567"
+conf /service/$site/af1/last-seen-at "$(TZ=UTC date -Iseconds)"
+conf /service/$site/foo2/listen-address "localhost:4568"
+conf /service/$site/foo2/last-seen-at "$(TZ=UTC date -Iseconds)"
--- src/merlion/core.clj
+++ src/merlion/core.clj
@@ -1,15 +1,136 @@
 (ns merlion.core
-  (:require [merlion.etcd :as etcd])
+  (:require [merlion.etcd :as etcd]
+            [clojure.pprint :refer [pprint]]
+            [manifold.stream :as s]
+            [clojure.string :as str]
+            [clojure.set :as set]
+            [clojure.core.async :as async
+             :refer [<! go chan >! close! alts!]]
+            [aleph.http :as http])
   (:gen-class))
 
-(defn update-frontend [_ _ _ new-state]
-  ;; note this is called on the agent thread so it'd be good if it doesn't block
-  (println "new configuration" new-state))
+(defn refresh-backend-watcher [watcher old-prefix new-prefix]
+  (if (and watcher (= old-prefix new-prefix))
+    watcher
+    (do (if watcher (close! watcher))
+        (etcd/watch-prefix new-prefix))))
+
+(defn proxy-handler1 [backends-ch req]
+  (println [:req req :ch backends-ch] )
+  (println :L)
+  (let [ch (chan)]
+    (go (async/put! backends-ch req)
+        (dotimes [i 20]
+          #_ (<! (async/timeout 500))
+          (>! ch (.toString i)))
+        (close! ch))
+    {:status 200 :headers {"content-type" "text/plain"}
+     :body (s/->source ch)}))
+
+(defn proxy-handler [ch req]
+  (proxy-handler1 ch req))
+
+(defn parse-address [a]
+  (let [[address port] (str/split a #":")]
+    {:address address :port (Integer/parseInt port)}))
+
+
+(defn make-backend-channel [backend]
+  (let [ch (chan)]
+    (go
+      (loop []
+        (when-let [r (<! ch)]
+          (println [:got-backend-req
+                    (parse-address (:listen-address backend))
+                    r])
+          (<! (async/timeout 1000))
+          (recur )))
+      (println [:shutdown backend]))
+    ch))
+
+(defn update-listeners-from-config [ch current new-address]
+  ;; open server sockets for all the addresses in config that aren't
+  ;; in listeners; drain and close all the sockets in
+  ;; listeners which aren't in config.
+  ;; we only have one listener right now, so this takes
+  ;; some shortcuts
+  (if new-address
+    (if (get current new-address)
+      current
+      (do
+        ;; need to check this isn't breaking in-flight requests
+        (when-let [old (first (vals current))]
+          (println [:closing old])
+          (.close old))
+        {new-address
+         (http/start-server (partial proxy-handler ch)
+                            (parse-address  new-address))}))
+    current))
+
+(defn update-backends-from-config [current new-backends]
+  (let [before (set (keys current))
+        after (set (keys new-backends))
+        deleted (set/difference before after)
+        create (set/difference after before)]
+    (dorun (map (fn [b] (close! (:channel (get current b)))) deleted))
+    (reduce-kv (fn [m k v]
+                 (assoc m k
+                        (if (:channel v)
+                          v
+                          (assoc v :channel
+                                 (make-backend-channel v)))))
+               {}
+               (apply dissoc new-backends deleted))))
+
+(defn print-state [l b]
+  (pprint {:listeners l :backends b}))
+
+(defn run-server [prefix]
+  (let [shutdown-ch (chan)
+        config-watcher (etcd/watch-prefix prefix)
+        backends-ch (chan)]
+    (go
+      (loop [listeners {}
+             backends {}
+             backend-prefix nil
+             backend-watcher nil]
+        (let [config (etcd/get-prefix prefix)
+              new-bp (:upstream-service-etcd-prefix config)
+              backends (if new-bp
+                         (update-backends-from-config
+                          backends (etcd/get-prefix new-bp))
+                         backends)
+              listeners (update-listeners-from-config
+                         backends-ch
+                         listeners
+                         (:listen-address config))]
+          (print-state listeners backends)
+          (let [[val ch]
+                (alts! (remove nil?
+                               [config-watcher
+                                backend-watcher
+                                shutdown-ch
+                                backends-ch]))]
+            (println [:inclong_from ch val])
+            (if (= ch backends-ch)
+              (println ["Incoming!" val]))
+            (if (= ch shutdown-ch)
+              (.close (first (vals listeners)))
+              (recur listeners
+                     backends
+                     new-bp
+                     (refresh-backend-watcher backend-watcher backend-prefix new-bp))))))
+      (println "quit"))
+    shutdown-ch))
+
+
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (add-watch etcd/configuration :update-frontend update-frontend)
-  (etcd/watch-config (first args))
+;  (add-watch etcd/configuration :update-frontend update-frontend)
+;  (etcd/watch-config (first args))
   (println "running"))
