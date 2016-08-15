da08993e5d7dc76405875988713d6c64da899f5c
Daniel Barlow <dan@telent.net>
2016-08-15 15:58:06 +0100

    + needs url encoding

--- seed-conf.sh
+++ seed-conf.sh
@@ -12,11 +12,12 @@ if [ "$1" = "clean" ] ; then
 fi
 
 conf $prefix/upstream-service-etcd-prefix /service/$site
-conf $prefix/upstream-freshness 60
+conf $prefix/upstream-freshness 10
 conf $prefix/log-format edn
 conf $prefix/listen-address localhost:8088
 
 #conf /service/$site/afs1/listen-address "localhost:4567"
 #conf /service/$site/afs1/last-seen-at "$(TZ=UTC date -Iseconds)"
-conf /service/$site/fg/listen-address "localhost:8000" 
-conf /service/$site/fg/last-seen-at "$(TZ=UTC date -Iseconds)"
+conf /service/$site/fg1/listen-address "localhost:8001"
+conf /service/$site/fg1/last-seen-at "$(TZ=UTC date -Iseconds| sed 's/+/%2b/' )"
+
