7af7f1e1d1ef1f13a96fced7bbb06a546ac2a759
Daniel Barlow <dan@telent.net>
2017-01-08 22:33:40 +0000

    first passing test
    Some less-than-desirable greebles in the implementation
    
    * Thread/sleep to give the server time to come online
    * server port may well clash with some other process, need better way to
     choose this

---
+++ test/fixtures/README.md
@@ -0,0 +1,5 @@
+# Quasi-legal note
+
+Text files in this directory were obtained from Project Gutenberg
+(which project has no connection or association with this software)
+and are believed (by me) to be freely redistributable
--- test/merlion/core_test.clj
+++ test/merlion/core_test.clj
@@ -1,6 +1,10 @@
 (ns merlion.core-test
+  (:import [java.net InetAddress Socket])
   (:require [me.raynes.conch.low-level :as shell]
             [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.core.async :as async]
+            [merlion.core :as core]
             [clojure.test :as test :refer [deftest testing is]]
             ))
 
@@ -16,4 +20,56 @@
             (assoc p :port (Integer/parseInt (last m)))
             (recur (rest lines))))))))
 
-#_ (socat {:source "file:README.md"})
+(def port (if-let [p (System/getenv "TESTPORT")]
+            (Integer/parseInt p)
+            8088))
+
+(def domain-name "test.merlion.telent.net")
+(def prefix
+  (let [n (.getName (java.lang.management.ManagementFactory/getRuntimeMXBean))]
+    (str "/" (str/replace n #"[^a-zA-Z0-9]" "_"))))
+
+(defn seed-etcd []
+  (doseq
+      [c [["etcdctl" "rm" "--recursive"
+           (str prefix "/conf/merlion/" domain-name)]
+          ["etcdctl" "set"
+           (str prefix "/conf/merlion/" domain-name "/upstream-freshness")
+           "3600"]
+          ["etcdctl" "set"
+           (str prefix "/conf/merlion/" domain-name "/upstream-service-etcd-prefix")
+           (str prefix "/service/" domain-name)]
+          ["etcdctl" "set"
+           (str prefix "/conf/merlion/" domain-name "/listen-address")
+           (str "localhost:" port)]]]
+    (print (shell/stream-to-string (apply shell/proc c) :out))))
+
+(def server (atom nil))
+
+(defn setup-fixtures [f]
+  (seed-etcd)
+  (reset! server (core/run-server (str prefix "/conf/merlion/" domain-name)))
+  (f)
+  (async/put! @server :quit))
+
+(test/use-fixtures :once setup-fixtures)
+
+(defn etcdctl [name value]
+  (let [c ["/usr/bin/env" "etcdctl" "set" (str prefix "/" name) value]]
+    (print (shell/stream-to-string (apply shell/proc c) :out))))
+
+(defn tcp-slurp [host port]
+  (let [s (Socket. host port)]
+    (slurp (io/reader (.getInputStream s)))))
+
+(deftest download
+  (testing "small file transfer"
+    (let [be-process (socat {:source "file:test/fixtures/excerpt.txt"})]
+      (etcdctl (str "service/" domain-name "/aaa/listen-address")
+               (str "localhost:" (:port be-process)))
+      (etcdctl (str "service/" domain-name "/aaa/last-seen-at")
+               (.toString (java.time.Instant/now)))
+      (Thread/sleep 500)
+      (is (= (slurp "test/fixtures/excerpt.txt")
+             (tcp-slurp (InetAddress/getLoopbackAddress) port)))
+      )))
