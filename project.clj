ec71ea5b7155bdd7e8a32fa4afa07089bd8c4e82
Daniel Barlow <dan@telent.net>
2017-01-13 13:10:56 +0000

    upgrade to clojure 1.9, for spec support

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
 (defproject merlion "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "etcd-based TCP proxy"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [me.raynes/conch "0.8.0"
                   :exclusions [org.clojure/clojure]]
-                 [aleph "0.4.1"]
+                 [aleph "0.4.2-alpha10"]
                  [cheshire "5.6.3"]
-                 [ring "1.5.1"]
+                 [ring "1.6.0-beta6"  :exclusions [org.clojure/java.classpath]]
                  [com.taoensso/timbre "4.8.0"]
                  [org.clojure/core.async "0.2.395"]
+                 [org.clojure/test.check "0.9.0"]
                  [ring/ring-codec "1.0.1"]]
   :plugins [[lein-ancient "0.6.10"]]
   :main ^:skip-aot merlion.core
